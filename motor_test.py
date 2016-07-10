b677be8e71eb17d7271ca830f68a3256a88fcf94
Rezaan Syed <r5syed@uwaterloo.ca>
2016-07-10 01:53:01 -0400

    Motor controller demo

---
+++ motor_test.py
@@ -0,0 +1,11 @@
+import serial
+import time
+
+s = serialport = serial.Serial("/dev/ttyAMA0", 9600, timeout=0.5)
+s.write( chr(0xAA) + chr(0x09) + chr(0x08) + chr(50) )  # motor 0 full speed forward
+time.sleep(1)
+s.write( chr(0xAA) + chr(0x09) + chr(0x08) + chr(0) )    # motor 0 speed to 0
+time.sleep(1)
+s.write( chr(0xAA) + chr(0x09) + chr(0x0A) + chr(50) )    # motor 0 full speed reverse
+time.sleep(1)
+s.write( chr(0xAA) + chr(0x09) + chr(0x0A) + chr(0) )      # motor 0 stop
