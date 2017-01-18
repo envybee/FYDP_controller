250ef2116bd19269b0e159a299c0f4364577afee
sagardave <davesagar2012@gmail.com>
2017-01-17 20:57:55 -0500

    add methods for turns

--- recieve_inputs.py
+++ recieve_inputs.py
@@ -1,11 +1,14 @@
-
+from MotorController import MCInterface
 import threading
 from msvcrt import getch
+
+CONST_VELOCITY = 5
 
 class Inputs(threading.Thread):
     def __init__(self, data_queue, threadID):
         self.data_queue = data_queue
         self.establish_connection()
+        self.mc = MCInterface()
 
         threading.Thread.__init__(self)
         self.threadID = threadID
@@ -22,6 +25,23 @@ class Inputs(threading.Thread):
                 print("left") #Left Arrow
             elif key == 77:
                 print("right") #Right Arrow
+
+    def turn_left():
+        self.mc.forwardM0(CONST_VELOCITY)
+        self.mc.forwardM0(CONST_VELOCITY)
+
+    def turn_right():
+        self.mc.reverseM1(CONST_VELOCITY)
+        self.mc.reverseM1(CONST_VELOCITY)
+
+    def forward():
+        self.mc.forwardM0(CONST_VELOCITY)
+        self.mc.reverseM1(CONST_VELOCITY)
+
+    def reverse():
+        normVel = abs(int(2*CONST_VELOCITY))
+        self.mc.reverseM0(normVel)
+        self.mc.forwardM1(normVel)
 
     def establish_connection(self):
         pass
