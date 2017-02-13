68db935fe0c3d613fc4a902d38d895ea3a28bd87
sagardave <davesagar2012@gmail.com>
2017-02-12 23:09:48 -0500

    change max to 40

--- Infinite_Motor_Drive.py
+++ Infinite_Motor_Drive.py
@@ -3,12 +3,12 @@ from time import sleep
 
 mc = MCInterface()
 
-for i in range(0, 20, 3):
+for i in range(0, 40, 3):
 	mc.forwardM0(i)
 	mc.forwardM1(i)
 	sleep(1)
 
-for i in range(20, 0, -3):
+for i in range(40, 0, -3):
 	mc.forwardM0(i)
 	mc.forwardM1(i)
 	sleep(1)
