c3800a8e70eb30480f300243ebde12ebc823013d
sagardave <davesagar2012@gmail.com>
2017-02-12 22:58:06 -0500

    test loop velocity change

--- Infinite_Motor_Drive.py
+++ Infinite_Motor_Drive.py
@@ -2,6 +2,13 @@ from MotorController import MCInterface
 
 mc = MCInterface()
 
-while True:
-	mc.forwardM0(149)
-	mc.forwardM1(149)
+for i in range(0, 149, 5):
+	mc.forwardM0(i)
+	mc.forwardM1(i)
+
+for i in range(149, 0, -5):
+	mc.forwardM0(i)
+	mc.forwardM1(i)
+
+mc.forwardM0(0)
+mc.forwardM1(0)
