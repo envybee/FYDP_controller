5dc203b60a63344cf3ab99e1708e706dabe499ca
sagardave <davesagar2012@gmail.com>
2017-02-12 23:01:06 -0500

    add sleep

--- Infinite_Motor_Drive.py
+++ Infinite_Motor_Drive.py
@@ -1,14 +1,17 @@
 from MotorController import MCInterface
+from time import sleep
 
 mc = MCInterface()
 
 for i in range(0, 149, 5):
 	mc.forwardM0(i)
 	mc.forwardM1(i)
+	time.sleep(0.2)
 
 for i in range(149, 0, -5):
 	mc.forwardM0(i)
 	mc.forwardM1(i)
+	time.sleep(0.2)
 
 mc.forwardM0(0)
 mc.forwardM1(0)
